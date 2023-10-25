f295f60dd0f5a08192383769bf03250305a5d66b
Aliaksandr Zelianko <zely1984@gmail.com>
2023-10-25 23:47:09 +0300

    add first screen

--- numerology/ContentView.swift
+++ numerology/ContentView.swift
@@ -9,13 +9,116 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack(spacing: 8) {
+                VStack{
+                    Text("Доп.числа")
+                        .font(.custom("AvenirNext-Bold", size: 12))
+                        .foregroundColor(.white)
+                    Text("15,6,11,2")
+                        .font(.custom("AvenirNext-Bold", size: 18))
+                        .foregroundColor(.white)
+                }
+                .frame(width: screen.width/2.1,
+                       height: screen.width/5)
+                    .background(Color.cyan.opacity(0.63))
+                    .cornerRadius(12)
+                
+                VStack{
+                    Text("Число судьбы")
+                        .font(.custom("AvenirNext-Bold", size: 12))
+                        .foregroundColor(.white)
+                    Text("8")
+                        .font(.custom("AvenirNext-Bold", size: 18))
+                        .foregroundColor(.white)
+                }
+                .frame(width: screen.width/4.4,
+                           height: screen.width/5)
+                    .background(Color.cyan.opacity(0.63))
+                    .cornerRadius(12)
+            
+            VStack{
+                Text("Темперамент")
+                    .font(.custom("AvenirNext-Bold", size: 12))
+                    .foregroundColor(.white)
+                Text("18")
+                    .font(.custom("AvenirNext-Bold", size: 18))
+                    .foregroundColor(.white)
+            }
+            .frame(width: screen.width/4.4,
+                       height: screen.width/5)
+                .background(Color.cyan.opacity(0.63))
+                .cornerRadius(12)
         }
-        .padding()
+            
+            
+            HStack(spacing: 8) {
+                VStack{
+                    Text("Характер")
+                        .font(.custom("AvenirNext-Bold", size: 12))
+                        .foregroundColor(.white)
+                    Text("15")
+                        .font(.custom("AvenirNext-Bold", size: 18))
+                        .foregroundColor(.white)
+                }
+                .frame(width: screen.width/4.4,
+                           height: screen.width/5)
+                    .background(Color.cyan.opacity(0.63))
+                    .cornerRadius(12)
+                
+                VStack{
+                    Text("Здоровье")
+                        .font(.custom("AvenirNext-Bold", size: 12))
+                        .foregroundColor(.white)
+                    Text("8")
+                        .font(.custom("AvenirNext-Bold", size: 18))
+                        .foregroundColor(.white)
+                }
+                .frame(width: screen.width/4.4,
+                           height: screen.width/5)
+                    .background(Color.cyan.opacity(0.63))
+                    .cornerRadius(12)
+            
+            VStack{
+                Text("Удача")
+                    .font(.custom("AvenirNext-Bold", size: 12))
+                    .foregroundColor(.white)
+                Text("18")
+                    .font(.custom("AvenirNext-Bold", size: 18))
+                    .foregroundColor(.white)
+            }
+            .frame(width: screen.width/4.4,
+                       height: screen.width/5)
+                .background(Color.cyan.opacity(0.63))
+                .cornerRadius(12)
+                
+                VStack{
+                    Text("Цель")
+                        .font(.custom("AvenirNext-Bold", size: 12))
+                        .foregroundColor(.white)
+                    Text("18")
+                        .font(.custom("AvenirNext-Bold", size: 18))
+                        .foregroundColor(.white)
+                }
+                .frame(width: screen.width/4.4,
+                           height: screen.width/5)
+                    .background(Color.cyan.opacity(0.63))
+                    .cornerRadius(12)
+        }
+            
+        }.frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity)
+        .background(LinearGradient(
+            colors: [.blue, .orange],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        ))
+        
+        
+        
     }
 }
 
