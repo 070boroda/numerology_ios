212aac25c5d6276d1536b6f9848c4223b389e700
zely20 <zely20@mail.ru>
2023-10-25 21:46:35 +0300

    Initial Commit

---
+++ numerology/numerologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  numerologyApp.swift
+//  numerology
+//
+//  Created by Alexandr on 25.10.23.
+//
+
+import SwiftUI
+
+@main
+struct numerologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
