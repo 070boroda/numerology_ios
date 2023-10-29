1fcd4df93c64556da76b0f069a3fa10392754f70
Aliaksandr Zelianko <zely1984@gmail.com>
2023-10-29 11:30:01 +0300

    add second screen

--- numerology/view/CompatibilityView.swift
+++ numerology/view/CompatibilityView.swift
@@ -1,18 +1,18 @@
 //
-//  CompobilitiView.swift
+//  CompatibilityView.swift
 //  numerology
 //
-//  Created by Alexandr on 27.10.23.
+//  Created by Alexandr on 29.10.23.
 //
 
 import SwiftUI
 
-struct CompobilitiView: View {
+struct CompatibilityView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CompobilitiView()
+    CompatibilityView()
 }
