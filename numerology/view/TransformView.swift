497200a91dedcd6b713751a352c3e911c3f1cbd1
Aliaksandr Zelianko <zely1984@gmail.com>
2023-10-29 20:45:49 +0300

    add transformation

---
+++ numerology/view/TransformView.swift
@@ -0,0 +1,18 @@
+//
+//  TransformView.swift
+//  numerology
+//
+//  Created by Alexandr on 29.10.23.
+//
+
+import SwiftUI
+
+struct TransformView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransformView()
+}
---
+++ numerology/viewmodel/TransformCounrterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransformCounrterView.swift
+//  numerology
+//
+//  Created by Alexandr on 29.10.23.
+//
+
+import Foundation
