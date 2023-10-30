1fedcdde15c8d2c63dac1547815c35be8a401209
Aliaksandr Zelianko <zely1984@gmail.com>
2023-10-30 18:57:18 +0300

    add degradations

---
+++ numerology/view/DegradationView.swift
@@ -0,0 +1,18 @@
+//
+//  DegradationView.swift
+//  numerology
+//
+//  Created by Alexandr on 30.10.23.
+//
+
+import SwiftUI
+
+struct DegradationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DegradationView()
+}
---
+++ numerology/viewmodel/DegradationCounrterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DegradationCounrterViewModel.swift
+//  numerology
+//
+//  Created by Alexandr on 30.10.23.
+//
+
+import Foundation
