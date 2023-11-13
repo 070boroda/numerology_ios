6a10a2140d1e0912154d0bff3f6a92352bbe4a26
Aliaksandr Zelianko <zely1984@gmail.com>
2023-11-13 20:19:48 +0300

    add local

---
+++ numerology/view/HelpView.swift
@@ -0,0 +1,18 @@
+//
+//  HelpView.swift
+//  numerology
+//
+//  Created by Alexandr on 2.11.23.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpView()
+}
